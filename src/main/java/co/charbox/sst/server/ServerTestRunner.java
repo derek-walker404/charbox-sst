f25481515bc430b3c9e6c61d2c269622de96dda1
David Esposito <david@charbox.co>
2015-07-11 07:16:17 -0400

    'Moving common sst functionality out of client.'

--- src/main/java/co/charbox/sst/server/ServerTestRunner.java
+++ src/main/java/co/charbox/sst/server/ServerTestRunner.java
@@ -12,18 +12,18 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.joda.time.DateTime;
 
-import co.charbox.client.sst.InvalidDeviceTokenException;
-import co.charbox.client.sst.SSTProperties;
-import co.charbox.client.sst.results.SstResultsHandler;
-import co.charbox.client.sst.utils.DataReceiver;
-import co.charbox.client.sst.utils.DataSender;
-import co.charbox.client.sst.utils.MyIOHAndler;
+import co.charbot.sst.InvalidDeviceTokenException;
+import co.charbot.sst.results.SstResultsHandler;
+import co.charbot.sst.utils.DataReceiver;
+import co.charbot.sst.utils.DataSender;
+import co.charbot.sst.utils.MyIOHAndler;
 import co.charbox.core.utils.SpeedUtils;
 import co.charbox.domain.model.DeviceModel;
 import co.charbox.domain.model.SstResultsModel;
 import co.charbox.domain.model.mm.ConnectionInfoModel;
 import co.charbox.domain.model.mm.ConnectionModel;
 import co.charbox.domain.model.mm.SimpleLocationModel;
+import co.charbox.domain.properties.SSTProperties;
 
 @Slf4j
 @Builder
