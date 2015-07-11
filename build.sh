1424767063d2063aac3932ac1083a3e01d969f08
David Esposito <david@charbox.co>
2015-07-11 07:33:58 -0400

    'Fixing build script'

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 cd ~/Workspace/java/tpofof-core/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-domain/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-core/;mvn clean install -DskipTests
-cd ~/Workspace/java/charbox-sst-common/;mvn clean install -DskipTests
+cd ~/Workspace/java/charbot/charbot-sst-common/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-sst/;mvn clean install -DskipTests
