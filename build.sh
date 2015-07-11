c1c7a92c414d77b6e25f304fa48b3a8ce2ccfcc3
David Esposito <david@charbox.co>
2015-07-11 09:31:33 -0400

    'Adding installation support'

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 cd ~/Workspace/java/tpofof-core/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-domain/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-core/;mvn clean install -DskipTests
-cd ~/Workspace/java/charbox-client/;mvn clean install -DskipTests
+cd ~/Workspace/java/charbox-sst-common/;mvn clean install -DskipTests
 cd ~/Workspace/java/charbox-sst/;mvn clean install -DskipTests
